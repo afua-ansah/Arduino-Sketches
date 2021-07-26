383fa30cbdb828a175a65ebb9943a67a6d290abb
afua-ansah <ama376@cornell.edu>
2021-07-26 14:11:21 -0400

    Update traffic_light.ino
    changed order to red, green, yellow

--- traffic_light/traffic_light.ino
+++ traffic_light/traffic_light.ino
@@ -15,15 +15,16 @@ digitalWrite(red, HIGH); //turn red light on
 delay(19500); // remains on for 19.5 s
 digitalWrite(red, LOW); //turn red light off
 
+//turn green on
+digitalWrite(green, HIGH);
+delay(20000);
+digitalWrite(green, LOW);
+
 //turn yellow on momentarily
 digitalWrite(yellow, HIGH);
 delay(10000);
 digitalWrite(yellow,LOW);
 
-//turn green on
-digitalWrite(green, HIGH);
-delay(20000);
-digitalWrite(green, LOW);
 
 
 }
