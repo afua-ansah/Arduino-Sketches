1374cc399cb48cdbc14947b04737ee29deaa19cf
afua-ansah <ama376@cornell.edu>
2021-07-25 21:09:18 -0400

    Create traffic_light.ino
    first commit of sketch of traffic light

---
+++ traffic_light/traffic_light.ino
@@ -0,0 +1,29 @@
+int red = 3;
+int yellow = 5;
+int green = 7;
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(red, OUTPUT);
+pinMode(yellow, OUTPUT);
+pinMode(green, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(red, HIGH); //turn red light on
+delay(19500); // remains on for 19.5 s
+digitalWrite(red, LOW); //turn red light off
+
+//turn yellow on momentarily
+digitalWrite(yellow, HIGH);
+delay(10000);
+digitalWrite(yellow,LOW);
+
+//turn green on
+digitalWrite(green, HIGH);
+delay(20000);
+digitalWrite(green, LOW);
+
+
+}
